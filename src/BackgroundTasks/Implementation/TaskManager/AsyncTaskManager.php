858aa755b5e057f8c9fadf9c8739484a0b1fbeba
fneumann <fneumann@databay.de>
2024-09-30 11:56:40 +0200

    Fix #42183: Async background tasks fail silently

--- src/BackgroundTasks/Implementation/TaskManager/AsyncTaskManager.php
+++ src/BackgroundTasks/Implementation/TaskManager/AsyncTaskManager.php
@@ -68,9 +68,14 @@ class AsyncTaskManager extends BasicTaskManager
         );
 
         try {
-            $soap_client->call(self::CMD_START_WORKER, array(
+            $result = $soap_client->call(self::CMD_START_WORKER, array(
                 $session_id . '::' . $client_id,
             ));
+            if ($result === false) {
+                $DIC->logger()->root()->info("[BT] Calling Webserver returned false, fallback to sync version");
+                $sync_manager = new SyncTaskManager($this->persistence);
+                $sync_manager->run($bucket);
+            }
         } catch (\Throwable $t) {
             $DIC->logger()->bgtk()->warning($t->getMessage());
             $DIC->logger()->bgtk()->warning("Calling webserver failed, fallback to sync version");
