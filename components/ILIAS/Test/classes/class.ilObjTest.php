c77c3054c53f2b77218dd00b52a753e17a0f5016
Stephan Kergomard <webmaster@kergomard.ch>
2024-10-21 13:11:58 +0200

    Test: Improvements after QS

--- components/ILIAS/Test/classes/class.ilObjTest.php
+++ components/ILIAS/Test/classes/class.ilObjTest.php
@@ -6695,7 +6695,7 @@ class ilObjTest extends ilObject
 
         $path = $this->export_factory->getExporter(
             $this,
-            ExportImportTypes::SCORED_RUN
+            ExportImportTypes::SCORED_ATTEMPT
         )->withFilterByActiveId($active_id)
             ->withResultsPage()
             ->withUserPages()
