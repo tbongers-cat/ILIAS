b83834275988a1d04b7a04246576b3f867736b10
Fabian Helfer <fhelfer@databay.de>
2024-10-29 16:00:26 +0100

    Fix Certificate Template Saving with not migrated global Certificate Background Signed Image Paths

--- components/ILIAS/Certificate/classes/User/class.ilUserCertificateGUI.php
+++ components/ILIAS/Certificate/classes/User/class.ilUserCertificateGUI.php
@@ -169,7 +169,9 @@ class ilUserCertificateGUI
                 if ($thumbnail_image_identification === '' || $thumbnail_image_identification === '-') {
                     $thumbnail_image_identification = $certificateData['thumbnail_image_path'] ?? '';
                     if ($thumbnail_image_identification !== '' && $this->filesystem->has($thumbnail_image_identification)) {
-                        $imagePath = ilFileUtils::getWebspaceDir() . $thumbnail_image_identification;
+                        $imagePath = ilWACSignedPath::signFile(
+                            ilFileUtils::getWebspaceDir() . $thumbnail_image_identification
+                        );
                     }
                 } else {
                     $thumb_rid = $this->irss->manage()->find($thumbnail_image_identification);
@@ -179,7 +181,7 @@ class ilUserCertificateGUI
                 }
 
                 if ($imagePath === '') {
-                    $imagePath = ilUtil::getImagePath('standard/icon_cert.svg');
+                    $imagePath = ilWACSignedPath::signFile(ilUtil::getImagePath('standard/icon_cert.svg'));
                 }
 
                 $cardImage = $this->uiFactory->image()->standard(
