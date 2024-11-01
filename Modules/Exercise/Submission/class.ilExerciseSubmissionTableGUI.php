d68105cf942e91652b846d43a41bc193afad9bcd
Alexander Killing <killing@leifos.de>
2024-11-01 09:42:03 +0100

    42531: Access to Submissions and Grades crashes if assignments have been started but nothing handed in yet

--- Modules/Exercise/Submission/class.ilExerciseSubmissionTableGUI.php
+++ Modules/Exercise/Submission/class.ilExerciseSubmissionTableGUI.php
@@ -457,7 +457,7 @@ abstract class ilExerciseSubmissionTableGUI extends ilTable2GUI
                     // see #22076
                     $this->tpl->setVariable(
                         "VAL_" . strtoupper($col),
-                        ilLegacyFormElementsUtil::prepareFormOutput(trim((string) $a_row[$col]))
+                        ilLegacyFormElementsUtil::prepareFormOutput(trim((string) ($a_row[$col] ?? "")))
                     );
                     break;
 
@@ -466,7 +466,7 @@ abstract class ilExerciseSubmissionTableGUI extends ilTable2GUI
                     $this->tpl->setVariable("LCOMMENT_ID", $comment_id . "_snip");
 
                     // see #22076
-                    $this->tpl->setVariable("VAL_" . strtoupper($col), (trim((string) $a_row[$col]) !== "")
+                    $this->tpl->setVariable("VAL_" . strtoupper($col), (trim((string) ($a_row[$col] ?? "")) !== "")
                         ? nl2br(trim((string) $a_row[$col]))
                         : "&nbsp;");
                     break;
