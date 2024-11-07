d54bf6389333948fbb407c51a78ac9ad9fd22bd8
Alex Killing <killing@leifos.de>
2024-11-07 16:35:38 +0100

    42605: Style der erweiteterten Tabelle führt zum Fehler

--- components/ILIAS/Awareness/classes/class.ilAwarenessGUI.php
+++ components/ILIAS/Awareness/classes/class.ilAwarenessGUI.php
@@ -93,7 +93,8 @@ class ilAwarenessGUI implements ilCtrlBaseClassInterface
     {
         $ilUser = $this->user;
         // init js
-        $this->main_tpl->addJavaScript("./assets/js/Awareness.js");
+        $this->main_tpl->addJavaScript("../components/ILIAS/Awareness/resources/Awareness.js");
+        //$this->main_tpl->addJavaScript("./assets/js/Awareness.js");
         $this->ctrl->setParameter($this, "ref_id", $this->ref_id);
         $this->main_tpl->addOnLoadCode("il.Awareness.setBaseUrl('" . $this->ctrl->getLinkTarget(
             $this,
