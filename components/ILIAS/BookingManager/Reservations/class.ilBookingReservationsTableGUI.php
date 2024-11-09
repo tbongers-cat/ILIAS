63e54fc7c69b513218dc3b76923b4e6d5db70d35
Alexander Killing <killing@leifos.de>
2024-11-09 12:18:24 +0100

    42618: Bei der Aktivierung von zusätzlichen Spalten (Personenbezogenen Daten) erfolgt eine ODF Fehlermeldung.

--- components/ILIAS/BookingManager/Reservations/class.ilBookingReservationsTableGUI.php
+++ components/ILIAS/BookingManager/Reservations/class.ilBookingReservationsTableGUI.php
@@ -109,6 +109,8 @@ class ilBookingReservationsTableGUI extends ilTable2GUI
         $this->addColumn("", "", 1);
         $this->addColumn($this->lng->txt("title"), "title");
 
+        $this->lng->loadLanguageModule("crs");
+
         $selected = $this->getSelectedColumns();
         $cols = $this->getSelectableColumns();
 
@@ -559,10 +561,11 @@ class ilBookingReservationsTableGUI extends ilTable2GUI
                     $user_ids = array_diff($user_ids, ilMemberAgreement::lookupAcceptedAgreements($parent_obj_id));
                 }
                 $odf_data = ilCourseUserData::_getValuesByObjId($parent_obj_id);
-
                 $usr_data = [];
                 foreach ($odf_data as $usr_id => $fields) {
-                    if (in_array($usr_id, $user_ids, true)) {
+                    // this currently does not with strict mode, since
+                    // $user_ids holds strings
+                    if (in_array($usr_id, $user_ids)) {
                         foreach ($fields as $field_id => $value) {
                             if (in_array($field_id, $odf_ids, true)) {
                                 $usr_data[$usr_id]['odf_' . $field_id] = $value;
@@ -701,7 +704,7 @@ class ilBookingReservationsTableGUI extends ilTable2GUI
         foreach ($this->getSelectedColumns() as $col) {
             if (isset($user_cols[$col])) {
                 $this->tpl->setCurrentBlock("user_col");
-                $this->tpl->setVariable("VALUE_USER_COL", $a_set[$col] . " ");
+                $this->tpl->setVariable("VALUE_USER_COL", ($a_set[$col] ?? "") . " ");
                 $this->tpl->parseCurrentBlock();
             }
         }
