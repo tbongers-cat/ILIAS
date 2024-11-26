c20c11471acf0dfc5cbb76aa76a78c6813b01b0d
Fabian Helfer <82493694+fhelfer@users.noreply.github.com>
2024-11-26 16:16:24 +0100

    Certificate Bugfix #0042876: Fix Saving Certificate with <br> is no valid XHTML (#8511)

--- components/ILIAS/Certificate/classes/Form/class.ilFormFieldParser.php
+++ components/ILIAS/Certificate/classes/Form/class.ilFormFieldParser.php
@@ -91,7 +91,7 @@ class ilFormFieldParser
             }
         }
 
-        $xsl = file_get_contents(__DIR__ . '/../../xml/xhtml2fo.xsl');
+        $xsl = file_get_contents(__DIR__ . '/../../xml/fo2xhtml.xsl');
         if ($content !== '' && (is_string($xsl) && $xsl !== '')) {
             $args = [
                 '/_xml' => $content,
