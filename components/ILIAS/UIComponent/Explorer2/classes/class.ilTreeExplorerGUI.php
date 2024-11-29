4a70e51807762dd47e902d646b5eabdb5a6c231a
Stephan Kergomard <webmaster@kergomard.ch>
2024-11-29 08:53:58 +0100

    RepositoryExplorer: Respect isClickable/isVisible
    See: https://mantis.ilias.de/view.php?id=42905

--- components/ILIAS/UIComponent/Explorer2/classes/class.ilTreeExplorerGUI.php
+++ components/ILIAS/UIComponent/Explorer2/classes/class.ilTreeExplorerGUI.php
@@ -235,7 +235,8 @@ abstract class ilTreeExplorerGUI extends ilExplorerBaseGUI implements \ILIAS\UI\
 
         $final_childs = [];
         foreach ($childs as $k => $c) {
-            if ($this->matches($c) || !$this->isNodeRequested($a_parent_node_id)) {
+            if ($this->isNodeVisible($c)
+                && ($this->matches($c) || !$this->isNodeRequested($a_parent_node_id))) {
                 $final_childs[$k] = $c;
             }
         }
@@ -382,7 +383,7 @@ abstract class ilTreeExplorerGUI extends ilExplorerBaseGUI implements \ILIAS\UI\
         $node = $this->createNode($factory, $record);
 
         $href = $this->getNodeHref($record);
-        if ($href !== '' && '#' !== $href) {
+        if ($href !== '' && '#' !== $href && $this->isNodeClickable($record)) {
             $node = $node->withLink(new \ILIAS\Data\URI(ILIAS_HTTP_PATH . '/' . $href));
         }
 
