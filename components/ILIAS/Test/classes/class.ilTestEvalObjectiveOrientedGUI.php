026ff82831f6398ec242b60caef5af84be57e592
Stephan Kergomard <webmaster@kergomard.ch>
2024-12-04 12:20:39 +0100

    Test: Fix Access to Missing Class Property
    See: https://mantis.ilias.de/view.php?id=43052

--- components/ILIAS/Test/classes/class.ilTestEvalObjectiveOrientedGUI.php
+++ components/ILIAS/Test/classes/class.ilTestEvalObjectiveOrientedGUI.php
@@ -82,7 +82,7 @@ class ilTestEvalObjectiveOrientedGUI extends ilTestServiceGUI
         }
 
         $this->toolbar->addComponent(
-            $this->ui->factory()->button()->standard($this->lng->txt('print'), '')
+            $this->ui_factory->button()->standard($this->lng->txt('print'), '')
                 ->withOnLoadCode(fn($id) => "$('#$id').on('click', ()=>{window.print();})")
         );
 
