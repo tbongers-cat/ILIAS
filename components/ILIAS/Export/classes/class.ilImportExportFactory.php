e06b8e021f9cd4f3cd196c3fe3593597b8b23012
Tim Schmitz <104776863+schmitz-ilias@users.noreply.github.com>
2024-12-20 09:41:50 +0100

    Merge pull request #8770 from leifos-gmbh/10_mantis_ilias_42781
    Export: mantis ilias 42781

--- components/ILIAS/Export/classes/class.ilImportExportFactory.php
+++ components/ILIAS/Export/classes/class.ilImportExportFactory.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * This file is part of ILIAS, a powerful learning management system
  * published by ILIAS open source e-Learning e.V.
@@ -17,6 +15,8 @@ declare(strict_types=1);
  * https://github.com/ILIAS-eLearning
  *
  *********************************************************************/
+
+declare(strict_types=1);
 
 /**
  * Factory for importer/exporter implementers
@@ -87,6 +87,10 @@ class ilImportExportFactory
      */
     public static function getImporterClass(string $a_component): string
     {
+        # Compatibility with older exports
+        # Object changed to ILIASObject with the component revision
+        $a_component = "components/ILIAS/Object" ? "components/ILIAS/ILIASObject" : $a_component;
+
         /**
          * @var $objDefinition ilObjectDefinition
          */
