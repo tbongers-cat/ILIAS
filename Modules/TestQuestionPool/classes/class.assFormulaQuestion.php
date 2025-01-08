2f58f742e05d1672510fb232ab9c436d8661fed0
Stephan Kergomard <webmaster@kergomard.ch>
2025-01-08 15:59:18 +0200

    Revert "TA: 43313, fix calculations of points (#8757)"
    This reverts commit ca733c89d4cce0ea27e2a06c7fbd8d00e5323012.

--- Modules/TestQuestionPool/classes/class.assFormulaQuestion.php
+++ Modules/TestQuestionPool/classes/class.assFormulaQuestion.php
@@ -883,8 +883,12 @@ class assFormulaQuestion extends assQuestion implements iQuestionCondition, ilAs
     }
 
     /**
-     * Returns the points a learner has reached by answering the question
+     * Returns the points, a learner has reached answering the question
      * The points are calculated from the given answers.
+     *
+     * @param integer $user_id The database ID of the learner
+     * @param integer $test_id The database Id of the test containing the question
+     * @access public
      */
     public function calculateReachedPoints($active_id, $pass = null, $authorizedSolution = true, $returndetails = false): float
     {
@@ -892,27 +896,33 @@ class assFormulaQuestion extends assQuestion implements iQuestionCondition, ilAs
             $pass = $this->getSolutionMaxPass($active_id);
         }
         $solutions = $this->getSolutionValues($active_id, $pass, $authorizedSolution);
-
-        foreach ($solutions as $key => $value) {
-            if (preg_match("/^(\\\$v\\d+)$/", $key, $matches)) {
-                $varObj = $this->getVariable($key);
-                $varObj->setValue($value);
-            }
-            if (preg_match("/^(\\\$r\\d+)_unit$/", $key, $matches)) {
-                if (!array_key_exists($matches[1], $solutions)) {
-                    $solutions[$matches[1]] = [];
+        $user_solution = array();
+        foreach ($solutions as $idx => $solution_value) {
+            if (preg_match("/^(\\\$v\\d+)$/", $solution_value["value1"], $matches)) {
+                $user_solution[$matches[1]] = $solution_value["value2"];
+                $varObj = $this->getVariable($solution_value["value1"]);
+                $varObj->setValue($solution_value["value2"]);
+            } elseif (preg_match("/^(\\\$r\\d+)$/", $solution_value["value1"], $matches)) {
+                if (!array_key_exists($matches[1], $user_solution)) {
+                    $user_solution[$matches[1]] = array();
                 }
-                $solutions[$matches[1]]["unit"] = $value;
+                $user_solution[$matches[1]]["value"] = $solution_value["value2"];
+            } elseif (preg_match("/^(\\\$r\\d+)_unit$/", $solution_value["value1"], $matches)) {
+                if (!array_key_exists($matches[1], $user_solution)) {
+                    $user_solution[$matches[1]] = array();
+                }
+                $user_solution[$matches[1]]["unit"] = $solution_value["value2"];
             }
         }
-
+        //vd($this->getResults());
         $points = 0;
         foreach ($this->getResults() as $result) {
+            //vd($user_solution[$result->getResult()]["value"]);
             $points += $result->getReachedPoints(
                 $this->getVariables(),
                 $this->getResults(),
-                $solutions[$result->getResult()]["value"] ?? '',
-                $solutions[$result->getResult()]["unit"] ?? '',
+                $user_solution[$result->getResult()]["value"] ?? '',
+                $user_solution[$result->getResult()]["unit"] ?? '',
                 $this->unitrepository->getUnits()
             );
         }
@@ -1267,7 +1277,7 @@ class assFormulaQuestion extends assQuestion implements iQuestionCondition, ilAs
      */
     public function getBestSolution($solutions): array
     {
-        $user_solution = [];
+        $user_solution = array();
 
         foreach ($solutions as $idx => $solution_value) {
             if (preg_match("/^(\\\$v\\d+)$/", $solution_value["value1"], $matches)) {
@@ -1286,7 +1296,6 @@ class assFormulaQuestion extends assQuestion implements iQuestionCondition, ilAs
                 $user_solution[$matches[1]]["unit"] = $solution_value["value2"];
             }
         }
-
         foreach ($this->getResults() as $result) {
             $resVal = $result->calculateFormula($this->getVariables(), $this->getResults(), parent::getId(), false);
 
@@ -1303,12 +1312,12 @@ class assFormulaQuestion extends assQuestion implements iQuestionCondition, ilAs
                 $check_unit = false;
                 if (array_key_exists($result_name, $available_units) &&
                     $available_units[$result_name] !== null) {
-                    $check_unit = in_array($user_solution[$result_name]['value']['unit'], $available_units[$result_name]);
+                    $check_unit = in_array($user_solution[$result_name]['unit'], $available_units[$result_name]);
                 }
 
                 if ($check_unit == true) {
                     //get unit-factor
-                    $unit_factor = assFormulaQuestionUnit::lookupUnitFactor($user_solution[$result_name]['value']['unit']);
+                    $unit_factor = assFormulaQuestionUnit::lookupUnitFactor($user_solution[$result_name]['unit']);
                 }
 
                 try {
