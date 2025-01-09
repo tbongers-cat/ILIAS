2c0d583b67a559769e74eaade9c802c2c1796072
Alexander Killing <killing@leifos.de>
2025-01-09 10:13:08 +0100

    fixed 42276: no certificate creation in excisting courses (service initialisation)

--- components/ILIAS/Container/Service/class.InternalService.php
+++ components/ILIAS/Container/Service/class.InternalService.php
@@ -24,22 +24,21 @@ use ILIAS\DI\Container;
 
 class InternalService
 {
-    protected Container $DIC;
-    protected array $instance = [];
+    protected static array $instance = [];
 
-    public function __construct(Container $DIC)
-    {
-        $this->DIC = $DIC;
+    public function __construct(
+        protected Container $DIC
+    ) {
     }
 
     public function data(): InternalDataService
     {
-        return $this->instance["data"] ??= new InternalDataService();
+        return self::$instance["data"] ??= new InternalDataService();
     }
 
     public function repo(): InternalRepoService
     {
-        return $this->instance["repo"] ??= new InternalRepoService(
+        return self::$instance["repo"] ??= new InternalRepoService(
             $this->data(),
             $this->DIC->database()
         );
@@ -47,16 +46,16 @@ class InternalService
 
     public function domain(): InternalDomainService
     {
-        return $this->instance["domain"] ??= new InternalDomainService(
+        return self::$instance["domain"] ??= new InternalDomainService(
             $this->DIC,
             $this->repo(),
-            $this->data()
+            $this->data(),
         );
     }
 
     public function gui(): InternalGUIService
     {
-        return $this->instance["gui"] ??= new InternalGUIService(
+        return self::$instance["gui"] ??= new InternalGUIService(
             $this->DIC,
             $this->data(),
             $this->domain()
