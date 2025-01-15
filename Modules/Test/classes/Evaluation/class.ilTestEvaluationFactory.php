2701011dc12c0d62f13561250e6bbc6b1c2358dc
Stephan Kergomard <webmaster@kergomard.ch>
2025-01-15 11:43:02 +0200

    Test: Fix Error on Null Title
    See: https://mantis.ilias.de/view.php?id=43627

--- Modules/Test/classes/Evaluation/class.ilTestEvaluationFactory.php
+++ Modules/Test/classes/Evaluation/class.ilTestEvaluationFactory.php
@@ -147,7 +147,7 @@ class ilTestEvaluationFactory
                 $user_eval_data = new ilTestEvaluationUserData($scoring_settings);
 
                 $user_eval_data->setName(
-                    $this->buildName($row['usr_id'], $row['firstname'], $row['lastname'], $row['title'])
+                    $this->buildName($row['usr_id'], $row['firstname'], $row['lastname'], $row['title'] ?? '')
                 );
 
                 if ($row['login'] !== null) {
