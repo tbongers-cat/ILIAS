950eba274baa19997dd06e9b4465ab4048d52751
Tim Schmitz <104776863+schmitz-ilias@users.noreply.github.com>
2025-01-20 14:16:40 +0100

    Merge pull request #8726 from schmitz-ilias/10_search_remove_split_button
    Search: replace split button (43226)

--- components/ILIAS/Search/classes/class.ilRepositorySearchGUI.php
+++ components/ILIAS/Search/classes/class.ilRepositorySearchGUI.php
@@ -262,33 +262,25 @@ class ilRepositorySearchGUI
         }
 
         $clip = ilUserClipboard::getInstance($user->getId());
+
+        $add_button = ilSubmitButton::getInstance();
+        $add_button->setCaption($a_options['submit_name'], false);
+        $add_button->setCommand('addUserFromAutoComplete');
+
+        if (!$a_sticky || $clip->hasContent()) {
+            $toolbar->addButtonInstance($add_button);
+        } else {
+            $toolbar->addStickyItem($add_button);
+        }
+
         if ($clip->hasContent()) {
-            $action_button = ilSplitButtonGUI::getInstance();
-
-            $add_button = ilSubmitButton::getInstance();
-            $add_button->setCaption($a_options['submit_name'], false);
-            $add_button->setCommand('addUserFromAutoComplete');
-
-            $action_button->setDefaultButton($add_button);
-
             $clip_button = ilSubmitButton::getInstance();
             $clip_button->addCSSClass('btn btndefault');
             $lng->loadLanguageModule('user');
             $clip_button->setCaption($lng->txt('clipboard_add_from_btn'), false);
             $clip_button->setCommand('showClipboard');
 
-            $action_button->addMenuItem(new ilButtonToSplitButtonMenuItemAdapter($clip_button));
-
-            $toolbar->addButtonInstance($action_button);
-        } else {
-            $button = ilSubmitButton::getInstance();
-            $button->setCaption($a_options['submit_name'], false);
-            $button->setCommand('addUserFromAutoComplete');
-            if (!$a_sticky) {
-                $toolbar->addButtonInstance($button);
-            } else {
-                $toolbar->addStickyItem($button);
-            }
+            $toolbar->addButtonInstance($clip_button);
         }
 
         if ($a_options['add_search'] ||
