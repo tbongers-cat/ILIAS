15309e1101ce3d1a759eadb52d8bfe67f0f2259c
Alex Killing <killing@leifos.de>
2025-01-22 16:24:05 +0100

    43795: Objects Cannot Be Deleted Due to Missing Function in ilPortfolioAppEventListener

--- components/ILIAS/Portfolio/classes/class.ilPortfolioAppEventListener.php
+++ components/ILIAS/Portfolio/classes/class.ilPortfolioAppEventListener.php
@@ -28,13 +28,6 @@ class ilPortfolioAppEventListener
         array $parameter
     ): void {
         switch ($component) {
-            case "components/ILIAS/ILIASObject":
-                switch ($event) {
-                    case "beforeDeletion":
-                        self::beforeDeletion($parameter);
-                        break;
-                }
-                break;
             case "components/ILIAS/User":
                 switch ($event) {
                     case "firstLogin":
