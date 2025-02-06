8f0f68bc0eb657df53e62084c2aa90718ecbfa46
Alex Killing <killing@leifos.de>
2025-02-06 12:54:58 +0100

    fixed strong/imp combination and similar cases

--- components/ILIAS/COPage/Editor/js/src/components/paragraph/ui/tiny-wrapper.js
+++ components/ILIAS/COPage/Editor/js/src/components/paragraph/ui/tiny-wrapper.js
@@ -987,7 +987,7 @@ export default class TinyWrapper {
       t = 'mycode';
     }
     ed.execCommand('mceToggleFormat', false, t);
-    this.getTinyDomTransform().removeMultiClasses(`ilc_text_inline_${t}`);
+    this.getTinyDomTransform().nestMultiClasses(`ilc_text_inline_${t}`);
     ed.focus();
     // ed.selection.collapse(false); // see #33963
     this.autoResize();
